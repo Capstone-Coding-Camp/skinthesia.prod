c2ba3b7b25867757cbbb2c2add3fbfdd37e51d53
XaveriusYehuda <xyehuda3@gmail.com>
2025-06-13 16:57:39 +0700

    sql lagi backend

--- utils/connection.js
+++ utils/connection.js
@@ -1,9 +1,9 @@
 // Konfigurasi database
 const dbConfig = {
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'skinthesia', // Ganti dengan nama database Anda
+    host: process.env.DB_HOST || 'database-2.cts6c2eoukli.ap-southeast-1.rds.amazonaws.com',
+    user: process.env.DB_USER || 'admin@gmail.com',
+    password: process.env.DB_PASSWORD || 'skinthesia123',
+    database: process.env.DB_NAME || 'database-2', // Ganti dengan nama database Anda
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
